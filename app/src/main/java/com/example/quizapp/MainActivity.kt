bb5dea9c9ce363327bf709b1fb8cbb31729f0a9c
Nikita Mieshalnykov <kot33adpot@gmail.com>
2020-01-19 16:19:51 -0400

    Comments.Constraint Layout. ASN is done.

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -6,12 +6,15 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * Author : Nikita Mieshalnykov
+ * Task : Assignment 01
+ */
+
 class MainActivity : AppCompatActivity() {
 
     lateinit var label: TextView
-
     private var index: Int = 0
-
     private val questionBank = listOf(
         Question(R.string.question_1, false),
         Question(R.string.question_2, true),
@@ -34,8 +37,8 @@ class MainActivity : AppCompatActivity() {
         Question(R.string.question_19, false),
         Question(R.string.question_20, true)
     )
-
     private var currentQuestion: Question = questionBank[index]
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,6 +46,9 @@ class MainActivity : AppCompatActivity() {
 
         label = findViewById(R.id.lbl)
         setQuestionTxt()
+
+
+        //Setting buttons and clickHandlers
 
         var trueButton: Button = findViewById(R.id.button_true)
         var falseButton: Button = findViewById(R.id.button_false)
@@ -54,9 +60,12 @@ class MainActivity : AppCompatActivity() {
         previousButton.setOnClickListener { prevQuestion() }
         trueButton.setOnClickListener { checkAnswer(true) }
         falseButton.setOnClickListener { checkAnswer(false) }
-
     }
 
+    /**
+     * Gets next question
+     * If out of range starts from the first one
+     */
     private fun nextQuestion() {
         if (index != questionBank.size - 1) {
             index++
@@ -67,6 +76,10 @@ class MainActivity : AppCompatActivity() {
         setQuestionTxt()
     }
 
+    /**
+     * Gets prev question
+     * If out of range starts from the last one
+     */
     private fun prevQuestion() {
         if (index != 0) {
             index--
@@ -77,10 +90,17 @@ class MainActivity : AppCompatActivity() {
         setQuestionTxt()
     }
 
+    /**
+     * Sets question text according to the current question
+     */
     private fun setQuestionTxt() {
         label.setText(currentQuestion.questionId)
     }
 
+    /**
+     * Checks users input and question answer
+     * Shows toast with the result
+     */
     private fun checkAnswer(inAnswer: Boolean) {
         var msg = ""
         if (inAnswer == currentQuestion.answer) {
